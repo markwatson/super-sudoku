a7fd8e965182a52ad3019143d11824908818a7fa
Mark Watson <me@markwatson.us>
2011-04-09 03:00:00 +0000

    Added arrow key navigation to the game boxes. Arrows are smart about what they can highlight.

--- SuperSudoku/MainWindow.xaml.cs
+++ SuperSudoku/MainWindow.xaml.cs
@@ -79,6 +79,205 @@ namespace SuperSudoku
             }
         }
 
+        /// <summary>
+        /// The grid GUI elements are labeled by grid section and grid element. This function
+        /// gets items to the left of a grid element.
+        /// 
+        /// Elements are in the form "_1x1" where the first number is the grid and the second is the element.
+        /// Returns itself on fail.
+        /// 
+        /// These algorithms follow a very computer sciency way to approach
+        /// the problem: screw math, just brute force it.
+        /// </summary>
+        private string GetLeftGridItem(string element)
+        {
+            string ret = element;
+
+            if (element.IndexOf('x') != -1 && element.IndexOf('_') == 0)
+            {
+                // first chop off the leading underscore and split by x, then put into ints
+                string[] elements = element.Substring(1).Split('x');
+                int grid = Convert.ToInt32(elements[0]);
+                int elem = Convert.ToInt32(elements[1]);
+
+                // if we're on the left side of the board
+                if (grid == 1 || grid == 4 || grid == 7)
+                {
+                    // and if we're not on the left side of the 9x9 grids
+                    if (!(elem == 1 || elem == 4 || elem == 7))
+                    {
+                        // subtract one from the element to move left
+                        elem--;
+                    }
+                }
+                // otherwise if we're in the center or right column of the board
+                else
+                {
+                    // and if we're not on the left side of the 9x9 grids
+                    if (!(elem == 1 || elem == 4 || elem == 7))
+                    {
+                        // subtract one from the element to move left
+                        elem--;
+                    }
+                    // otherwise we are on the left side
+                    else
+                    {
+                        // select the far right box on the column to the left
+                        elem += 2;
+                        grid--;
+                    }
+                }
+
+                ret = String.Format("_{0:d}x{1:d}", grid, elem);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// This function is similar to the GetLeftGridItem function, but gets the item to the right.
+        /// </summary>
+        private string GetRightGridItem(string element)
+        {
+            string ret = element;
+
+            if (element.IndexOf('x') != -1 && element.IndexOf('_') == 0)
+            {
+                // first chop off the leading underscore and split by x, then put into ints
+                string[] elements = element.Substring(1).Split('x');
+                int grid = Convert.ToInt32(elements[0]);
+                int elem = Convert.ToInt32(elements[1]);
+
+                // if we're on the right side of the board
+                if (grid == 3 || grid == 6 || grid == 9)
+                {
+                    // and if we're not on the right side of the 9x9 grids
+                    if (!(elem == 3 || elem == 6 || elem == 9))
+                    {
+                        // subtract one from the element to move left
+                        elem++;
+                    }
+                }
+                // otherwise if we're in the center or left column of the board
+                else
+                {
+                    // and if we're not on the right side of the 9x9 grids
+                    if (!(elem == 3 || elem == 6 || elem == 9))
+                    {
+                        // subtract one from the element to move right
+                        elem++;
+                    }
+                    // otherwise we are on the right side
+                    else
+                    {
+                        // select the far left box on the column to the right
+                        elem -= 2;
+                        grid++;
+                    }
+                }
+
+                ret = String.Format("_{0:d}x{1:d}", grid, elem);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// This function is similar to the GetLeftGridItem function, but gets the item above.
+        /// </summary>
+        private string GetAboveGridItem(string element)
+        {
+            string ret = element;
+
+            if (element.IndexOf('x') != -1 && element.IndexOf('_') == 0)
+            {
+                // first chop off the leading underscore and split by x, then put into ints
+                string[] elements = element.Substring(1).Split('x');
+                int grid = Convert.ToInt32(elements[0]);
+                int elem = Convert.ToInt32(elements[1]);
+
+                // if we're on the top of the board
+                if (grid == 1 || grid == 2 || grid == 3)
+                {
+                    // and if we're not on the very top of the 9x9 grids
+                    if (!(elem == 1 || elem == 2 || elem == 3))
+                    {
+                        // subtract 3 from the element to move up
+                        elem -= 3;
+                    }
+                }
+                // otherwise if we're in the center or bottom column of the board
+                else
+                {
+                    // and if we're not on the top of the 9x9 grids
+                    if (!(elem == 1 || elem == 2 || elem == 3))
+                    {
+                        // subtract three from the element to move up
+                        elem -= 3;
+                    }
+                    // otherwise we are on the top
+                    else
+                    {
+                        // select the bottom box on the column above
+                        elem += 6;
+                        grid -= 3;
+                    }
+                }
+
+                ret = String.Format("_{0:d}x{1:d}", grid, elem);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// This function is similar to the GetLeftGridItem function, but gets the item below.
+        /// </summary>
+        private string GetBelowGridItem(string element)
+        {
+            string ret = element;
+
+            if (element.IndexOf('x') != -1 && element.IndexOf('_') == 0)
+            {
+                // first chop off the leading underscore and split by x, then put into ints
+                string[] elements = element.Substring(1).Split('x');
+                int grid = Convert.ToInt32(elements[0]);
+                int elem = Convert.ToInt32(elements[1]);
+
+                // if we're on the bottom of the board
+                if (grid == 7 || grid == 8 || grid == 9)
+                {
+                    // and if we're not on the very bottom of the 9x9 grids
+                    if (!(elem == 7 || elem == 8 || elem == 9))
+                    {
+                        // add 3 to the element to move down
+                        elem += 3;
+                    }
+                }
+                // otherwise if we're in the center or top column of the board
+                else
+                {
+                    // and if we're not on the bottom of the 9x9 grids
+                    if (!(elem == 7 || elem == 8 || elem == 9))
+                    {
+                        // add three from the element to move down
+                        elem += 3;
+                    }
+                    // otherwise we are on the bottom
+                    else
+                    {
+                        // select the top box on the column below
+                        elem -= 6;
+                        grid += 3;
+                    }
+                }
+
+                ret = String.Format("_{0:d}x{1:d}", grid, elem);
+            }
+
+            return ret;
+        }
+
         // The following methods are GUI event handlers. The two arguments are the same for all event handlers.
         /// <summary>
         /// This function is called when the exit menu item is clicked.
@@ -149,6 +348,8 @@ namespace SuperSudoku
 
         private void GridElement_KeyDown(object sender, KeyEventArgs e)
         {
+            string name = ((TextBox)sender).Name;
+
             switch (e.Key)
             {
                 case Key.D1:
@@ -184,11 +385,26 @@ namespace SuperSudoku
                 case Key.Space:
                     ((TextBox)sender).Text = "";
                     break;
+                case Key.Left:
+                    // Calc left item and set it to be focused
+                    ((TextBox)FindName(GetLeftGridItem(name))).Focus();
+                    break;
+                case Key.Right:
+                    ((TextBox)FindName(GetRightGridItem(name))).Focus();
+                    break;
+                case Key.Up:
+                    ((TextBox)FindName(GetAboveGridItem(name))).Focus();
+                    break;
+                case Key.Down:
+                    ((TextBox)FindName(GetBelowGridItem(name))).Focus();
+                    break;
             }
         }
 
         private void GridElement_PreviewKeyDown(object sender, KeyEventArgs e)
         {
+            // We need to send these key presses down the line, otherwise
+            // they're blocked by this method.
             GridElement_KeyDown(sender, e);
         }
     }
